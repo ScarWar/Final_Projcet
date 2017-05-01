2db77b9a2b86cfc4dda09571fa08e65b59c26c06
matansokolovsky <matanso@tomobox.co>
2017-05-01 09:48:06 +0300

    config test passes

---
+++ unit_tests/sp_config_unit_test.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "unit_test_util.h" //SUPPORTING MACROS ASSERT_TRUE/ASSERT_FALSE etc..
+#include "../SPConfig.h"
+
+
+
+static bool goodConfigTest() {
+	SP_CONFIG_MSG msg;
+	SPConfig config = spConfigCreate("./unit_tests/goodconfig.config", &msg);
+    ASSERT_TRUE(msg == SP_CONFIG_SUCCESS);
+	return true;
+}
+
+static bool badConfigTest() {
+	SP_CONFIG_MSG msg;
+	SPConfig config = spConfigCreate("./unit_tests/badconfig.config", &msg);
+    ASSERT_TRUE(msg != SP_CONFIG_SUCCESS);
+	return true;
+}
+
+int main() {
+	RUN_TEST(goodConfigTest);
+	RUN_TEST(badConfigTest);
+	return 0;
+}
